147b8ed9952b93216650ae46792dfa537de752d5
nigaraliyevas <naliyeva965@gmail.com>
2023-12-27 01:39:06 +0400

    feat:Added DataContext layer

---
+++ CompanyApp.DataContext/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using CompanyApp.Domain.Models;
+
+namespace CompanyApp.DataContext
+{
+    public static class DbContext
+    {
+        public static List<Employee> Employees { get; set; }
+        public static List<Department> Departments { get; set; }
+        static DbContext()
+        {
+            Employees = new ();
+            Departments = new ();
+        }
+    }
+}
---
+++ CompanyApp.DataContext/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using CompanyApp.Domain.Models.Common;
+
+namespace CompanyApp.DataContext.Interfaces
+{
+    public interface IRepository<T> where T :BaseEntity
+    {
+        bool Create(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+        T Get (T entity);
+        List<T> GetAll(Predicate<T> entity = null);
+    }
+}
